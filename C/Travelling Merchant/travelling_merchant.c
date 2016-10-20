a1781a20b3c1ead579cc7169fad536d7c29ddf90
Dimitris Niras <niras.94@gmail.com>
2016-10-20 19:59:02 +0300

    Add files via upload

--- C/Travelling Merchant/travelling_merchant.c
+++ C/Travelling Merchant/travelling_merchant.c
@@ -1,81 +1,108 @@
 #include <stdio.h>
-#define MAX 100
+#include <stdlib.h>
+
+void next_vertex(int **connection,float **weights,int *degree,int kor_ek,int n,int *T,float sum_weight);
 
 int main()
 {
-    int i,j,N,kor_ek,temp,sum,adj[MAX][MAX],T[MAX],counter;
-    float sum_weight=0,min_weight,weight[MAX][MAX];
+    int i, j, n, kor_ek, **connection, *degree, *T;
+    float **weights, sum_weight=0;
 
-    do {
-        printf("Enter number of tops: ");
-        scanf("%d",&N);
-        if (N<=0 || N>MAX)
-            printf("Number of tops must be at (0,%d)\n",MAX);
-    } while (N<=0 || N>MAX);
-
-    for (i=0; i<N; i++){
-        for (j=0; j<N; j++) {
-            if (i==j)
-                adj[i][j]=0;
-            else {
-                do {
-                    printf("Give value 1 if there is a acme that connects top %d with top %d,\n",i,j);
-                    printf("or else give 0: ");
-                    scanf("%d",&adj[i][j]);
-                    if (adj[i][j]!=1 && adj[i][j]!=0)
-                        printf("Value must be 0 or 1!\n");
-                } while(adj[i][j]!=1 && adj[i][j]!=0);
-            }
-            if (adj[i][j]==1) {
-                printf("Enter weight of acme that connects top %d with %d: ",i,j);
-                scanf("%f",&weight[i][j]);
-            }
+    printf("Enter tops: ");
+    scanf("%d",&n);
+    if ((degree=(int *)malloc(n*sizeof(int)))==NULL) {
+        printf("Not enough memory in position 1.\n");
+        exit(1);
+    }
+    if ((connection=(int **)malloc(n*sizeof(int *)))==NULL) {
+        printf("Not enough memory in position 2.\n");
+        exit(2);
+    }
+    if ((weights=(float **)malloc(n*sizeof(float *)))==NULL) {
+        printf("Not enough memory in position 3.\n");
+        exit(3);
+    }
+    for (i=0; i<n; i++) {
+        printf("Enter rank of top %d : ",i);
+        scanf("%d",&degree[i]);
+        if ((connection[i]=(int *)malloc(degree[i]*sizeof(int)))==NULL) {
+            printf("Not enough memory in position 4.\n");
+            exit(4);
+        }
+        if ((weights[i]=(float *)malloc(degree[i]*sizeof(float)))==NULL) {
+            printf("Not enough memory in position 5.\n");
+            exit(5);
         }
     }
-
-    do {
-        printf("Enter start top: ");
-        scanf("%d",&kor_ek);
-        if (kor_ek<0 || kor_ek>=N)
-            printf("Start top must be in bounds 0 and %d.",N-1);
-    } while (kor_ek<0 || kor_ek>=N);
-
-    printf("Start top: %d\n",kor_ek);
-    printf("Path: %d",kor_ek);
+    if ((T=(int *)malloc(n*sizeof(int)))==NULL) {
+        printf("Not enough memory in position 6.\n");
+        exit(6);
+    }
+    for (i=0; i<n; i++) {
+        printf("Enter id of tops that connect with top %d.\n",i);
+        for (j=0; j<degree[i]; j++)
+            scanf("%d",&connection[i][j]);
+    }
+    for (i=0; i<n; i++) {
+        printf("Enter acme's weights that connect top %d.\n",i);
+        for (j=0; j<degree[i]; j++)
+            scanf("%f",&weights[i][j]);
+    }
+    printf("Enter start top: ");
+    scanf("%d",&kor_ek);
+    printf("Dtart top %d. ",kor_ek);
+    printf("Path %d ",kor_ek);
     T[kor_ek]=1;
-
-    do {
-        sum=0;
-        counter=0;
-        min_weight=100000;
-        for (j=0; j<N; j++) {
-            if (adj[kor_ek][j]==1) {
-                if (weight[kor_ek][j]<min_weight) {
-                    min_weight=weight[kor_ek][j];
-                    temp=j;
-                }
-            }
-        }
-        T[temp]=1;
-        sum_weight+=min_weight;
-        adj[kor_ek][temp]=0;
-        adj[temp][kor_ek]=0;
-        kor_ek=temp;
-        printf(",%d",kor_ek);
-        for (j=0; j<N; j++) {
-            sum+=adj[kor_ek][j];
-        }
-        for (i=0; i<N; i++) {
-            if (T[i]==1)
-                counter+=1;
-        }
-    } while (sum!=0 && counter<N);
-
-    if (sum!=0)
-        printf(". The route was completed with weight %f .",sum_weight);
-    else
-        printf(". Route did not completed.");
+    next_vertex(connection,weights,degree,kor_ek,n,T,sum_weight);
+    free(degree);
+    free(connection);
+    free(weights);
+    free(T);
 
     return 0;
 }
 
+void next_vertex(int **connection,float **weights,int *degree,int kor_ek,int n,int *T,float sum_weight)
+{
+    int i, j, sum=0, counter=0, temp;
+    float min_weight=1000000;
+
+    for (j=0; j<degree[kor_ek]; j++) {
+        if (connection[kor_ek][j]!=-1){
+            if (weights[kor_ek][j]<min_weight) {
+                min_weight=weights[kor_ek][j];
+                temp=connection[kor_ek][j];
+            }
+        }
+    }
+    sum_weight+=min_weight;
+    for (i=0; i<n; i++) {
+        for (j=0; j<degree[i]; j++) {
+            if (connection[i][j]==kor_ek) {
+                connection[i][j]=-1;
+            }
+        }
+    }
+    T[temp]=1;
+    kor_ek=temp;
+    printf("%d ",kor_ek);
+    for (j=0; j<degree[kor_ek]; j++) {
+        sum+=connection[kor_ek][j];
+    }
+    for (i=0; i<n; i++) {
+        if (T[i]==1) {
+            counter++;
+        }
+    }
+    if (counter==n) {
+        printf(". Cost %f. ",sum_weight);
+        printf("Path completed.\n");
+    }
+    else if (sum==(-1)*degree[kor_ek]) {
+        printf(". Cost %f. ",sum_weight);
+        printf("Path didn't completed.\n");
+    }
+    else
+        next_vertex(connection,weights,degree,kor_ek,n,T,sum_weight);
+
+}
